f2990d94d0b37c0048827d31cae84218cf061561
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-04-15 23:30:54 -0300

    var and const

---
+++ src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 3.4; // Cria variável
+        final double PI = 3.14159; // Cria constante
+
+        double area = PI * raio * raio;
+
+        System.out.println("Área = " + area + "m2.");
+
+    }
+}
